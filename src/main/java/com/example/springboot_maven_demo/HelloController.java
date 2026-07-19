2f55c61744081bfdf55669bc7b915fd68b8c211a
TharinsaBatagoda <biyonitharinsa@gmail.com>
2026-07-19 23:38:27 +0530

    Create Spring Boot Maven application

---
+++ src/main/java/com/example/springboot_maven_demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.springboot_maven_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello! This is my Spring Boot Maven application.";
+    }
+}
